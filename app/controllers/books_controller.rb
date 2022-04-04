5f5463029106803747a963c8b014c6d4a128ca7b
ramushu <ramushu1205@gmail.com>
2022-04-04 15:30:09 +0000

    [Update]4-task

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,18 @@
 class BooksController < ApplicationController
   def new
   end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to '/books'
+  end
 
   def index
+    @user = User.find(current_user.id)
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
@@ -10,4 +20,11 @@ class BooksController < ApplicationController
 
   def edit
   end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
