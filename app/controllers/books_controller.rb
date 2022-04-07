c30d2b82c694be4bc0517122fc2886a8b7948e1a
ramushu <ramushu1205@gmail.com>
2022-04-07 15:22:58 +0000

    [Update]4-task viewファイル完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,12 @@
 class BooksController < ApplicationController
   def new
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to '/books'
+    redirect_to books_path(@book.id)
   end
 
   def index
@@ -16,15 +16,32 @@ class BooksController < ApplicationController
   end
 
   def show
+    @user = User.find(current_user.id)
+    @book = Book.new
+    @userx = User.find(params[:id])
+    @bookx = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
-  
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
+  def destroy
+    @bookx = Book.find(params[:id])
+    @bookx.destroy
+    redirect_to books_path
+  end
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,32 @@
 class UsersController < ApplicationController
-  
+
   def index
-  
+    @user = User.find(current_user.id)
+    @book = Book.new
+    @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
-    
+    @user = User.find(current_user.id)
+    @book = Book.new
+    @userx = User.find(params[:id])
+    @bookx = Book.find(params[:id])
   end
 
   def edit
+    @user = User.find(current_user.id)
   end
+
+  def update
+    @user = User.find(current_user.id)
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
 end
